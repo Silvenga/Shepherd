870317e900380e0f5a56cbe06ca873b882e4491e
Mark Lopez <m@silvenga.com>
2021-02-10 22:21:43 -0600

    Added support for default settings locations.

--- src/Shepherd/Program.cs
+++ src/Shepherd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Lamar.Microsoft.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +13,11 @@ namespace Shepherd
             return Host.CreateDefaultBuilder(args)
                        .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Trace))
                        .UseLamar((_, registry) => registry.Include(new ShepherdRegistry()))
+                       .ConfigureAppConfiguration(builder =>
+                       {
+                           builder.AddJsonFile("/etc/vault-shepherd/settings.json", true);
+                           builder.AddJsonFile(@"C:\ProgramData\vault-shepherd\settings.json", true);
+                       })
                        .RunConsoleAsync();
         }
     }
